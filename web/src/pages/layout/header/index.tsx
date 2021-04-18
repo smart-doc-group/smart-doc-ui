081c51df98e3a3f65f17516fdb0d93e389c9a547
zixiCat <zixicat@qq.com>
2021-04-19 05:15:26 +0800

    fix(web/basic): romove test code and update eslint

--- web/src/pages/layout/header/index.tsx
+++ web/src/pages/layout/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Select, Typography, MenuItem } from '@material-ui/core';
@@ -21,8 +21,6 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
   const classes = useStyles();
-  const test = useTheme();
-  console.log(test);
 
   return (
     <>
